c62ecab9233a2189d8e1b6e627f44205d2b66912
Florian Geckeler <f.geckeler@sap.com>
2026-01-01 23:22:41 +0100

    Sketchybar: set bar size to 30

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,7 +2,7 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-    height = 35,
+    height = 30,
     -- y_offset = 30,
     color = colors.bar.bg,
     border_color = colors.bar.border,
